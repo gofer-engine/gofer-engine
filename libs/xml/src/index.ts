ba7b99e451f5f241b047ff8be394b897be7f5675
Anthony Master <themaster5.07@gmail.com>
2024-01-14 23:19:24 -0600

    fix(xml): refactor xml library WIP

--- libs/xml/src/index.ts
+++ libs/xml/src/index.ts
@@ -1,23 +1,31 @@
 import { Element, xml2js, js2xml, ElementCompact } from 'xml-js';
-import { cloneDeep, get, set, unset } from 'lodash';
+import { cloneDeep, set, unset } from 'lodash';
+// import jsdom from 'jsdom';
+
+import xpath, { SelectReturnType, SelectSingleReturnType } from 'xpath';
+import { DOMParser, XMLSerializer } from '@xmldom/xmldom';
+import { Xslt, XmlParser } from 'xslt-processor'
+
 
 import { IMsg, isMsg } from '@gofer-engine/message-type';
 
-import { parseXMLPath } from './parseXMLPath';
+// import { parseXMLPath } from './parseXMLPath';
+// import { getXML } from "./getXML";
+// import { parseXMLString } from "./parseXMLString";
 
-export { parseXMLPath };
+// export { parseXMLPath };
 
 export type XMLJson = Element;
 export type XMLJsonCompact = ElementCompact;
 
 export interface IXMLMsg extends IMsg {
   kind: 'XML';
-  setMsg: (msg: XMLJson) => IXMLMsg;
+  setMsg: (xml: Document | string) => IXMLMsg;
   json: <N extends boolean>(
     _normalized?: N,
   ) => N extends true ? XMLJson : XMLJsonCompact;
   set: (path?: string | undefined, value?: XMLJsonCompact) => IXMLMsg;
-  get: (path: string | undefined) => string | XMLJsonCompact;
+  get: (path: string | undefined) => Document | SelectReturnType | SelectSingleReturnType;
   delete: (path: string) => IXMLMsg;
   copy: (path: string, toPath: string) => IXMLMsg;
   move: (fromPath: string, toPath: string) => IXMLMsg;
@@ -42,52 +50,59 @@ export const isXMLMsg = (msg: unknown): msg is IXMLMsg => {
 export class XMLMsg implements IXMLMsg {
   readonly kind = 'XML';
   private _msg: XMLJson = {};
-  private compact = () =>
-    xml2js(js2xml(this._msg), { compact: true }) as XMLJsonCompact;
-  private uncompact = (msg: XMLJsonCompact) => {
-    this._msg = xml2js(js2xml(msg, { compact: true })) as XMLJson;
+  private _doc: Document;
+  private _serializer: XMLSerializer;
+  private _jsonify = () => xml2js(this.toString())
+  private _compact = () =>
+    xml2js(this.toString(), { compact: true }) as XMLJsonCompact;
+  private _uncompact = (msg: XMLJsonCompact) => {
+    this.setMsg(js2xml(msg, { compact: true }));
   };
-  private process = (fn: (msg: XMLJsonCompact) => XMLJsonCompact) => {
-    this.uncompact(fn(this.compact()));
+  private _process = (fn: (msg: XMLJsonCompact) => XMLJsonCompact) => {
+    this._uncompact(fn(this._compact()));
   };
-  constructor(msg?: string | XMLJson, parse = true) {
-    if (msg === '') {
-      this._msg = {};
-    } else if (parse) {
-      if (typeof msg === 'string') {
-        try {
-          this._msg = xml2js(msg, { 
-            compact: false,
-            elementNameFn: (value) => value.split(':').join('::'),
-          }) as XMLJson;
-        } catch (err) {
-          throw new Error(`Cannot parse string with xml2js: ${err}`);
-        }
-      } else {
-        throw new Error(`Cannot parse ${typeof msg} with xml2js`);
-      }
-    } else {
-      if (typeof msg === 'string') {
-        throw new Error(`Cannot create XMLMsg from string without parse`);
-      }
-      if (msg !== undefined) {
-        this._msg = cloneDeep(msg);
-      }
-    }
+  private _removeNamespaces = () => {
+    this.setMsg(this.toString().replace(/(?:\s)?xmlns(?::\w+)?="[^"]+"/g, ''))
+  };
+  // constructor(msg?: string | XMLJson) {
+  //   if (typeof msg === 'string') {
+  //     this._msg = parseXMLString(msg);
+  //   } else if (msg !== undefined) {
+  //     // NOTE: clone the msg so that we don't mutate the original nor external
+  //     // mutations affect us.
+  //     this._msg = cloneDeep(msg);
+  //   }
+  // };
+  constructor(xml: string) {
+    this._doc = new DOMParser().parseFromString(xml, 'text/xml');
+    this._serializer = new XMLSerializer();
   }
-  setMsg = (msg: XMLJson): IXMLMsg => {
-    this._msg = cloneDeep(msg);
+  // setMsg = (msg: XMLJson | string): IXMLMsg => {
+  //   if (typeof msg === 'string') {
+  //     this._msg = parseXMLString(msg);
+  //   } else {
+  //     this._msg = cloneDeep(msg);
+  //   }
+  //   return this;
+  // };
+  setMsg = (xml: Document | string): IXMLMsg => {
+    if (typeof xml === 'string') {
+      this._doc = new DOMParser().parseFromString(xml, 'text/xml');
+    } else {
+      // FIXME: how do we deeply clone a Document?
+      this._doc = xml;
+    }
     return this;
   };
   json = <N extends boolean>(
     normalized?: N,
   ): N extends true ? XMLJson : XMLJsonCompact => {
-    const msg = cloneDeep(this._msg);
-    return (normalized ? msg : this.compact()) as N extends true
+    return (normalized ? this._jsonify() : this._compact()) as N extends true
       ? XMLJson
       : XMLJsonCompact;
   };
-  toString = () => js2xml(this._msg, { spaces: 2 });
+  // toString = () => js2xml(this._msg, { spaces: 2 });
+  toString = () => this._serializer.serializeToString(this._doc);
   set = (
     path?: string,
     value?: string | XMLJsonCompact,
@@ -100,33 +115,106 @@ export class XMLMsg implements IXMLMsg {
       if (parse) {
         value = xml2js(value as string, { compact: true });
       }
-      this.process((msg) => set(msg, path, value));
+      this._process((msg) => set(msg, path, value));
     } else {
       throw new Error('Cannot set path on non-object');
     }
     return this;
   };
   setJSON = this.set;
-  get = <R>(path?: string): R => {
-    // TODO: decide if we want to return undefined instead of the entire
-    // message when no path is provided. How do we handle this with HL7v2?
-    // FIXME: Is there a better way than to cast to R?
-    if (path === undefined) return this.json() as R;
-    const valueObject = get(this.json(), path);
-    if (
-      typeof valueObject === 'object' &&
-      '_attributes' in valueObject &&
-      Object.keys(valueObject).length === 1
-    ) {
-      const attributes = valueObject._attributes;
-      if (typeof attributes === 'object' && 'value' in attributes) {
-        return attributes.value as R;
-      }
+  // parsePath = (path: string): (string | number)[] => parseXMLPath(path);
+  // get = <R extends XMLJson>(path?: string): R => getXML(this.json(true), this.parsePath(path)) as R
+  // getXML = (path: string) => js2xml(this.get(path) ?? {});
+  get = <R extends Document | SelectReturnType | SelectSingleReturnType>(path?: string, ns?: Record<string, string> | boolean): R => {
+    if (!path) {
+      return this._doc as R;
     }
-    return js2xml(valueObject, { compact: true, spaces: 2 }) as R;
-  };
+    if (ns === true) {
+      // TODO: find a better way to get all namespaces from the document that
+      // doesn't require serializing and regex over the whole string.
+      const namespaces: Record<string, string> = {}
+      const xml = this.toString();
+      const matches = xml.matchAll(/xmlns(?::(\w+))?="([^"]+)"/g)
+      for (const [_, nsKey, nsUri] of matches) {
+        if (nsKey) {
+          namespaces[nsKey] = nsUri;
+          continue;
+        }
+        const prefix = nsUri.split('/').pop();
+        namespaces[prefix] = nsUri;
+      }
+      return xpath.useNamespaces(namespaces)(path, this._doc) as R
+    }
+    if (ns === false) {
+      this._removeNamespaces();
+      // FIXME: Log warning of unsafe operation with potential data loss
+      // console.warn('WARNING: `ns` is set to false. This will remove any namespacing which risk name clashes and lose the ability to differentiate between once-distinct elements')
+    }
+    if (typeof ns === 'object' && Object.keys(ns).length) {
+      return xpath.useNamespaces(ns)(path, this._doc) as R
+    }
+    return xpath.select(path, this._doc) as R;
+  }
+  document = () => this._doc;
+  /**
+   * Transform the message with the provided XSLT.
+   */
+  applyXSLT = (xslt_string: string) => {
+    const xslt = new Xslt();
+    const xmlParser = new XmlParser();
+    const outXML = xslt.xsltProcess(
+      xmlParser.xmlParse(this.toString()),
+      xmlParser.xmlParse(xslt_string)
+    );
+    return outXML;
+    // this.setMsg(outXML);
+
+    // new Xslt().xsltProcess(
+    //   // xmlParse(this.toString()),
+    //   xmlParse(`<?xml version="1.0"?>
+    //     <root>
+    //       <foo xmlns="http://www.w3.org/1999/xhtml">
+    //         <bar value="Bar"/>
+    //         <baz>Baz</baz>
+    //       </foo>
+    //     </root>
+    //   `),
+    //   xmlParse(xslt)
+    // );
+
+    // return this;
+  }
+  // document = (cb: (doc: Document, writeBack: ()=> void) => void) => {
+  //   const DOM = new jsdom.JSDOM(this.toString(), { contentType: 'application/xhtml+xml' });
+  //   cb(DOM.window.document, () => {
+  //     this.setMsg(DOM.serialize());
+  //   });
+  //   /* FIXME: There is a curent limitation with jsdom serialization that
+  //    * excludes the xml declaration line. Example: `<?xml version="1.0"
+  //    * encoding="UTF-8"?>`. By using the document  Open Issue: https://github.com/jsdom/jsdom/issues/2615
+  //    */
+  //   return this;
+  // }
+  // get = <R>(path?: string): R => {
+  //   // TODO: decide if we want to return undefined instead of the entire
+  //   // message when no path is provided. How do we handle this with HL7v2?
+  //   // FIXME: Is there a better way than to cast to R?
+  //   if (path === undefined) return this.json() as R;
+  //   const valueObject = get(this.json(), path);
+  //   if (
+  //     typeof valueObject === 'object' &&
+  //     '_attributes' in valueObject &&
+  //     Object.keys(valueObject).length === 1
+  //   ) {
+  //     const attributes = valueObject._attributes;
+  //     if (typeof attributes === 'object' && 'value' in attributes) {
+  //       return attributes.value as R;
+  //     }
+  //   }
+  //   return js2xml(valueObject, { compact: true, spaces: 2 }) as R;
+  // };
   delete = (path: string): IXMLMsg => {
-    this.process((msg) => {
+    this._process((msg) => {
       unset(msg, path);
       return msg;
     });
@@ -152,37 +240,37 @@ export class XMLMsg implements IXMLMsg {
     v: string | Record<string, string> | string[] | (<T>(v: T, i: number) => T),
     { iteration }: { iteration?: boolean | undefined } = {},
   ): IXMLMsg => {
-    if (typeof this._msg !== 'object') {
-      throw new Error('Cannot map path on non-object msg');
-    }
-    const value = this.get(path);
-    if (Array.isArray(value)) {
-      if (typeof v === 'string') {
-        set(
-          this._msg,
-          path,
-          value.map(() => v),
-        );
-      } else if (Array.isArray(v)) {
-        set(
-          this._msg,
-          path,
-          value.map((_, i) => (iteration ? v?.[i % v.length] : v?.[i])),
-        );
-      } else if (typeof v === 'object') {
-        const val = value.map((val) => v?.[val as string]);
-        set(this._msg, path, val);
-      } else if (typeof v === 'function') {
-        const val = value.map(v);
-        set(this._msg, path, val);
-      } else {
-        throw new Error(
-          'Cannot map non-string, non-array, non-object, non-function',
-        );
-      }
-    } else {
-      throw new Error('Cannot map non-array');
-    }
+    // if (typeof this._msg !== 'object') {
+    //   throw new Error('Cannot map path on non-object msg');
+    // }
+    // const value = this.get(path);
+    // if (Array.isArray(value)) {
+    //   if (typeof v === 'string') {
+    //     set(
+    //       this._msg,
+    //       path,
+    //       value.map(() => v),
+    //     );
+    //   } else if (Array.isArray(v)) {
+    //     set(
+    //       this._msg,
+    //       path,
+    //       value.map((_, i) => (iteration ? v?.[i % v.length] : v?.[i])),
+    //     );
+    //   } else if (typeof v === 'object') {
+    //     const val = value.map((val) => v?.[val as string]);
+    //     set(this._msg, path, val);
+    //   } else if (typeof v === 'function') {
+    //     const val = value.map(v);
+    //     set(this._msg, path, val);
+    //   } else {
+    //     throw new Error(
+    //       'Cannot map non-string, non-array, non-object, non-function',
+    //     );
+    //   }
+    // } else {
+    //   throw new Error('Cannot map non-array');
+    // }
     return this;
   };
   setIteration = <Y>(
@@ -190,25 +278,25 @@ export class XMLMsg implements IXMLMsg {
     map: Y[] | ((val: Y, i: number) => Y),
     { allowLoop }: { allowLoop: boolean } = { allowLoop: true },
   ): IXMLMsg => {
-    if (typeof this._msg !== 'object') {
-      throw new Error('Cannot map path on non-object msg');
-    }
-    const value = this.get(path);
-    if (!Array.isArray(value)) {
-      throw new Error('Cannot map non-array');
-    }
-    if (Array.isArray(map)) {
-      set(
-        this._msg,
-        path,
-        value.map((_, i) => (allowLoop ? map?.[i % map.length] : map?.[i])),
-      );
-    } else if (typeof map === 'function') {
-      const val = value.map(map as any);
-      set(this._msg, path, val);
-    } else {
-      throw new Error('Cannot map non-array, non-function');
-    }
+    // if (typeof this._msg !== 'object') {
+    //   throw new Error('Cannot map path on non-object msg');
+    // }
+    // const value = this.get(path);
+    // if (!Array.isArray(value)) {
+    //   throw new Error('Cannot map non-array');
+    // }
+    // if (Array.isArray(map)) {
+    //   set(
+    //     this._msg,
+    //     path,
+    //     value.map((_, i) => (allowLoop ? map?.[i % map.length] : map?.[i])),
+    //   );
+    // } else if (typeof map === 'function') {
+    //   const val = value.map(map as any);
+    //   set(this._msg, path, val);
+    // } else {
+    //   throw new Error('Cannot map non-array, non-function');
+    // }
     return this;
   };
 }
--- libs/xml/src/xml.test.ts
+++ libs/xml/src/xml.test.ts
@@ -2,36 +2,95 @@ import { XMLMsg } from '.';
 import fs from 'fs';
 
 const patient = fs.readFileSync('./samples/sample.xml', 'utf8');
-const expected = fs.readFileSync('./samples/expected.xml', 'utf8');
 const json = fs.readFileSync('./samples/xml.json', 'utf8');
 const jsonCompacted = fs.readFileSync('./samples/xml_compact.json', 'utf8');
 
 const normalizeLE = (string: string) => string.replace(/\r\n|\r|\n/g, '\n');
 
-test('XMLMsg', () => {
-  expect(patient.substring(0, 5)).toBe('<?xml');
-  const msg = new XMLMsg(patient);
-  expect(msg).toBeInstanceOf(XMLMsg);
+describe('XMLMsg', () => {
+  test('read xml sample file', () => {
+    expect(patient.substring(0, 5)).toBe('<?xml');
+  })
+  let msg = new XMLMsg(patient);
+  test('can init class', () => {
+    expect(msg).toBeInstanceOf(XMLMsg);
   // NOTE: the line breakings are a tad different and inconsistent but equate to the same xml structure.
-  expect(normalizeLE(msg.toString())).toBe(normalizeLE(expected));
-  expect(msg.json()).toEqual(JSON.parse(jsonCompacted));
-  expect(msg.json(true)).toEqual(JSON.parse(json));
-  expect(msg.get('Patient.id')).toBe('example');
-  expect(msg.get('Patient.identifier.value')).toBe('12345');
-  expect(msg.get('Patient.text.div.table.tr[0].td[2]')).toBe('Deceased:');
-  expect(
-    msg
-      .set('Patient.text.div.table.tr[0].td[2]', 'Expired:')
-      .get('Patient.text.div.table.tr[0].td[2]'),
-  ).toBe('Expired:');
-  expect(msg.set('Patient.id', '456A').get('Patient.id')).toBe('456A');
-  expect(
-    msg
-      .set(
-        'Patient.name[0]',
-        '<use value="official"/><family value="Doe"/><given value="John"/>',
-        true,
-      )
-      .get('Patient.name[0].family'),
-  ).toBe('Doe');
+  });
+
+  describe('methods', () => {
+    beforeEach(() => {
+      msg = new XMLMsg(patient);
+    });
+
+    test('can stringify', () => {
+      expect(normalizeLE(msg.toString())).toBe(normalizeLE(patient));
+    });
+    test('can convert to compacted json', () => {
+      expect(msg.json()).toEqual(JSON.parse(jsonCompacted));
+    });
+    test('can convert to normalized json', () => {
+      expect(msg.json(true)).toEqual(JSON.parse(json));
+    });
+    test('can get path using local-name', () => {
+      expect(msg.get(`string(/*[local-name()='Patient']/*[local-name()='id']/@value)`).toString()).toBe("example");
+    });
+    test('can get path using manually defined prefix', () => {
+      expect(msg.get(`string(/foo:Patient/foo:id/@value)`, { foo: 'http://hl7.org/fhir', bar: 'http://www.w3.org/1999/xhtml' }).toString()).toBe("example");
+    });
+    test('can get path using auto defined prefixes', () => {
+      expect(msg.get(`string(/fhir:Patient/fhir:id/@value)`, true).toString()).toBe("example");
+    });
+    test('can get path removing namespaces', () => {
+      expect(msg.get(`string(/Patient/id/@value)`, false).toString()).toBe("example");
+    });
+    test('can apply XSLT tranformer', () => {
+      expect(msg.applyXSLT(`<?xml version="1.0"?>
+        <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+          <!-- TODO: add some transformation here -->
+        </xsl:stylesheet>
+      `).toString()).toBe('')
+    })
+    
+    // test('can use document api', () => {
+    //   msg.get<Document>().getElementsByTagName('id').item(0).setAttribute('value', 'e456A');
+    //   expect(msg.get('/Patient/id/@value')).toBe('e456A');
+    // });
+    // test('can use document api without writeBack', () => {
+    //   msg.document((doc) => {
+    //     doc.getElementsByTagName('id').item(0).setAttribute('value', 'e456A');
+    //     expect(doc.getElementsByTagName('id').item(0).getAttribute('value')).toBe('e456A');
+    //   })
+    //   expect(msg.getXML('/Patient/id@value')).toBe('example');
+    // });
+    // test('can get path as XML', () => {
+    //   expect(msg.get('/Patient/id@value')).toBe('example');
+    // });
+    // test('', () => {
+    //   expect(msg.getXML('/Patient/identifier/value[@value]')).toBe('12345');
+    // })
+    // test('', () => {
+    //   expect(msg.getXML('/Patient/text/div/table/tr[0]td[2]')).toBe('Deceased:');
+    // })
+    // test('can set and get path', () => {
+    //   expect(
+    //     msg
+    //       .set('Patient.text.div.table.tr[0].td[2]', 'Expired:')
+    //       .get('Patient.text.div.table.tr[0].td[2]'),
+    //   ).toBe('Expired:');
+    // })
+    // test('', () => {
+    //   expect(msg.set('Patient.id', '456A').get('Patient.id')).toBe('456A');
+    // });
+    // test('', () => {
+    //   expect(
+    //     msg
+    //       .set(
+    //         'Patient.name[0]',
+    //         '<use value="official"/><family value="Doe"/><given value="John"/>',
+    //         true,
+    //       )
+    //       .get('Patient.name[0].family'),
+    //   ).toBe('Doe');
+    // });
+  });
 });
